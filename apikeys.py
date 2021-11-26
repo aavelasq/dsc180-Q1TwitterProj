db1481b9805d4c64dbe5c5b271dbabdffb55a115
Abigail Velasquez <aavelasq@ucsd.edu>
2021-11-26 15:41:17 -0800

    added folders

---
+++ apikeys.py
@@ -0,0 +1,5 @@
+apikeys = {}
+apikeys['name'] = "dsc180a_b02"
+apikeys['key'] = "LNTXFm443GefLoopvf3kr5rjn"
+apikeys['secret'] = "9oFi38fHacTCwatloNXbQo5DpmreJlOwvOZgpkD1d39TWeBfKZ"
+apikeys['bearer_token'] = "AAAAAAAAAAAAAAAAAAAAAMh9VwEAAAAA2HJ5ua5a9gj1R3BVqewoBouPHkc%3DPjhGL3A3JrdgRLjCAMm8fqRl6h712BtxqSnXEGtks7AzIzYe1j"
