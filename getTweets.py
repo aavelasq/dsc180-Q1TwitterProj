dedf5cf19d2d61ec949fe5941ca75c5cee722090
Nikitha Gopal <49108547+nvgopal@users.noreply.github.com>
2021-11-29 21:40:11 -0800

    Add files via upload

--- getTweets.py
+++ getTweets.py
@@ -43,7 +43,7 @@ def main():
     date = datetime.datetime(2016, 6, 19)
     final_date = date + datetime.timedelta(days=61)
 
-    temp_date = date + timedelta(days=2)
+    # temp_date = date + timedelta(days=2)
 
     tweet_count = 0
     while date != final_date:
@@ -51,21 +51,22 @@ def main():
         end_date = date + datetime.timedelta(days=1)
         end_time = datetime.datetime.strftime(end_date, r"%Y-%m-%dT%H:%M:%SZ")
 
-        query_params = {'query': '"milo yiannopoulos" (#freemilo OR #miloyiannopoulos OR #freenero OR #milo OR #jesuismilo OR #yiannopoulos) -is:retweet', 
+        query_params = {'query': '"milo yiannopoulos" (#freemilo OR #miloyiannopoulos OR #freenero OR #milo OR #jesuismilo OR #yiannopoulos OR #milosd OR #nero OR #talktomilo OR #freemilonow OR #freethefaggot OR #dangerousfaggot OR #dangerousfaggottour OR #milosexual OR #miloatdepaul OR #bannero OR #jesuisnero OR #freespeechformillo OR #milobreakstheinternet OR #bringbackmilo OR #freemiloyiannopoulos OR #fuckmilo OR #suspendneroparty OR #chokemilo OR #milogirlsbreaktheinternet OR #banmilo OR #blockedformilo OR #verifymilo OR #miloatrutgers OR #standwithmilo OR #wheresmilo OR #prayformilo OR #teammilo) -is:retweet', 
                         'max_results': '100', 
                         'start_time': start_time, 
                         'end_time': end_time, 
                         'tweet.fields': 'created_at,author_id'}
 
         json_response = connect_to_endpoint(search_url, query_params)
-        df1 = pd.DataFrame(json_response['data'])
-        df = pd.concat([df, df1])
-        json_poutput = json.dumps(json_response, indent=4, sort_keys=True)
-
-        tweet_count += len(df1)
-
-        print('Tweet Gathered:', str(len(df)))
-        df.to_csv('/Users/nikithagopal/Documents/dsc30-pa1/dsc180-Q1TwitterProj/data/raw/rawData_milo.csv', index = False)
+        print(json_response)
+        if json_response['meta']['result_count'] != 0:
+            df1 = pd.DataFrame(json_response['data'])
+            df = pd.concat([df, df1])
+            json_poutput = json.dumps(json_response, indent=4, sort_keys=True)
+            
+            tweet_count += len(df1)
+            print('Tweet Gathered:', str(len(df)))
+            df.to_csv('/Users/nikithagopal/Documents/dsc30-pa1/dsc180-Q1TwitterProj/data/raw/rawData_milo.csv', index = False)
 
         time.sleep(3)
 
